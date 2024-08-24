1a3154d5c3e8fe56a559ef1e9e73ab1e51a666f1
Joseph Debao Chua <joseph.chua@arvato-scs.com>
2024-08-24 09:04:38 -0400

    Files added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:AIw5MzGhm6Mu9PoZWsJ6hUik@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
